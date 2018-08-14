78fed510b858a59d035b428b8a978956166dffdf
romuloufrpe <romulo.ferreira.ufrpe@gmail.com>
2018-08-14 17:51:06 -0300

    mudanças

--- api/user/authService.js
+++ api/user/authService.js
@@ -1,11 +1,9 @@
 const _ = require('lodash')
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
 const User = require('./user')
-const env = require('../../.env')
 
 const emailRegex = /\S+@\S+\.\S+/
-const passwordRegex = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})/
+const passwordRegex = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,12})/
+
 
 const sendErrorsFromDB = (res, dbErrors) => {
     const errors = []
@@ -16,17 +14,14 @@ const sendErrorsFromDB = (res, dbErrors) => {
 const login = (req, res, next) => {
     const email = req.body.email || ''
     const password = req.body.password || ''
-
     User.findOne({ email }, (err, user) => {
         if (err) {
             return sendErrorsFromDB(res, err)
-        } else if (user && bcrypt.compareSync(password, user.password)) {
-            const token = jwt.sign(user, env.authSecret, {
-                expiresIn: "1 day"
-            })
+        } else if (user && (password == user.password)) {
             const { name, email } = user
-            res.json({ name, email, token })
-        } else {
+            res.json({ name, email })
+        }
+        else {
             return res.status(400).send({ errors: ['Usuário/Senha inválidos'] })
         }
     })
@@ -34,7 +29,6 @@ const login = (req, res, next) => {
 
 const validateToken = (req, res, next) => {
     const token = req.body.token || ''
-
     jwt.verify(token, env.authSecret, function (err, decoded) {
         return res.status(200).send({ valid: !err })
     })
@@ -44,37 +38,22 @@ const signup = (req, res, next) => {
     const name = req.body.name || ''
     const email = req.body.email || ''
     const password = req.body.password || ''
-    const confirmPassword = req.body.confirm_password || ''
-
     if (!email.match(emailRegex)) {
         return res.status(400).send({ errors: ['O e-mail informado está inválido'] })
     }
-
     if (!password.match(passwordRegex)) {
-        return res.status(400).send({
-            errors: [
-                "Senha precisar ter: uma letra maiúscula, uma letra minúscula, um número, uma caractere especial(@#$ %) e tamanho entre 6-20."
-            ]
-        })
+        return res.status(400).send({ errors: ["Senha precisar ter: uma letra maiúscula, uma letra minúscula, um número, uma caractere especial(@#$%) e tamanho entre 6-12."] })
     }
-
-    const salt = bcrypt.genSaltSync()
-    const passwordHash = bcrypt.hashSync(password, salt)
-    if (!bcrypt.compareSync(confirmPassword, passwordHash)) {
-        return res.status(400).send({ errors: ['Senhas não conferem.'] })
-    }
-
     User.findOne({ email }, (err, user) => {
-        if (err) {
-            return sendErrorsFromDB(res, err)
-        } else if (user) {
+        if (err) { return sendErrorsFromDB(res, err) }
+        else if (user) {
             return res.status(400).send({ errors: ['Usuário já cadastrado.'] })
-        } else {
-            const newUser = new User({ name, email, password: passwordHash })
+        }
+        else {
+            const newUser = new User({ name, email, password })
             newUser.save(err => {
-                if (err) {
-                    return sendErrorsFromDB(res, err)
-                } else {
+                if (err) { return sendErrorsFromDB(res, err) }
+                else {
                     login(req, res, next)
                 }
             })
@@ -82,4 +61,5 @@ const signup = (req, res, next) => {
     })
 }
 
-module.exports = { login, signup, validateToken }
+
+module.exports = { login, validateToken,signup }
